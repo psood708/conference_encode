0ec08c5677b49257aef3d6533158edcf51caf200
Preet-Sojitra <preet.dev373@gmail.com>
2022-10-11 00:28:31 +0530

    fix broken responsiveness in sponser section

--- src/assets/data/carouseldata.js
+++ src/assets/data/carouseldata.js
@@ -13,25 +13,25 @@ const carouseldata = [
   },
   {
     key: 1,
-    mainHeading1: "Together we thrive",
-    mainHeading2: "achievements",
-    subTitle1: "Together We Thrive",
-    subTitle2: "Together We Thrive",
-    title: "Together We Thrive",
+    mainHeading1: "Lorem ipsum dolor",
+    mainHeading2: "sit",
+    subTitle1: "Lorem ipsum dolor",
+    subTitle2: "Lorem ipsum dolor",
+    title: "Lorem ipsum dolor",
     description:
-      "Recharge and reboot at SIGGRAPH 2019, as multifaceted as you are—featuring global innovators who are changing what’s possible in computer graphics, animation, VR, Gaming, and Emerging Technologies.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident .",
     imageSrc:
       "https://images.unsplash.com/photo-1665065960436-188a7471b1c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
   },
   {
     key: 2,
-    mainHeading1: "Together we thrive",
-    mainHeading2: "achievements",
-    subTitle1: "Together We Thrive",
-    subTitle2: "Together We Thrive",
-    title: "Together We Thrive",
+    mainHeading1: "Veritatis obcaecati tenetur",
+    mainHeading2: "iureeius",
+    subTitle1: "Veritatis obcaecati tenetur",
+    subTitle2: "Veritatis obcaecati tenetur",
+    title: "Veritatis obcaecati tenetur",
     description:
-      "Recharge and reboot at SIGGRAPH 2019, as multifaceted as you are—featuring global innovators who are changing what’s possible in computer graphics, animation, VR, Gaming, and Emerging Technologies.",
+      "earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid.",
     imageSrc:
       "https://images.unsplash.com/photo-1665076034878-1bf3eb03a853?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=60",
   },
