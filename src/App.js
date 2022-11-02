e2717434713ef25a64a9b31cdc100d53cbba268d
Parth <parth.sce21@sot.pdpu.ac.in>
2022-11-02 23:19:37 +0530

    Added Theme page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Section2 from "./Components/Section2"
 import Sponsor from "./Components/sponsor"
 import Footer from "./Components/footer"
 import Carousel from "./Components/Carousel"
+ 
 function App() {
   return (
-    <div className="landing-page">
-      <Navbar />
+    <div className="landing-page">  
+      <Navbar /> 
       <Main />
       <CountDown />
       <Section2 />
       <Carousel />
-      <Sponsor />
+      <Sponsor /> 
       <Footer />
     </div>
   )
