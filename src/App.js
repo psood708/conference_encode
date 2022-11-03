91b78ef8fafdb963785928f3a63f4247330489e2
Preet-Sojitra <preet.dev373@gmail.com>
2022-11-03 20:57:13 +0530

    an attempt to fix routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Contact from "./Components/Contact"
 import FirstPage from "./Pages/FirstPage"
 import React from "react"
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {Routes, Route, HashRouter} from "react-router-dom"
 import CommingSoon from "./Components/CommingSoon"
 import Committee from "./Components/Committee"
 import Theme from "./Components/Theme"
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path="/conference" element={<FirstPage />} />
         <Route exact path="/conference/committee" element={<Committee />} />
@@ -15,7 +15,7 @@ function App() {
         <Route exact path="/conference/commingsoon" element={<CommingSoon />} />
         <Route exact path="/conference/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
