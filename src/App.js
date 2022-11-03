b9366f463f82002fa29538779c503934812b3d08
Preet-Sojitra <preet.dev373@gmail.com>
2022-11-03 15:13:09 +0530

    fix broken routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,3 @@
-// // <<<<<<< HEAD
-// import Navbar from "./Components/Navbar"
-// import Main from "./Components/Main"
-// import CountDown from "./Components/CountDown"
-// import Section2 from "./Components/Section2"
-// import Sponsor from "./Components/sponsor"
-// import Footer from "./Components/footer"
-// import Carousel from "./Components/Carousel"
- 
-// =======
 import Contact from "./Components/Contact"
 import FirstPage from "./Pages/FirstPage"
 import React from "react"
@@ -15,7 +5,6 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import CommingSoon from "./Components/CommingSoon"
 import Committee from "./Components/Committee"
 import Theme from "./Components/Theme"
-// >>>>>>> c6472818015580578ce3c1d5b0fc9c5d13cf5276
 function App() {
   return (
     <BrowserRouter>
@@ -23,8 +12,8 @@ function App() {
         <Route exact path="/conference" element={<FirstPage />} />
         <Route exact path="/conference/committee" element={<Committee />} />
         <Route exact path="/conference/themes" element={<Theme />} />
-        <Route exact path="/conference/CommingSoon" element={<CommingSoon />} />
-        <Route exact path="/conference/Contact" element={<Contact />} />
+        <Route exact path="/conference/commingsoon" element={<CommingSoon />} />
+        <Route exact path="/conference/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   )
