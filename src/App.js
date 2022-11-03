90d412998406c592d3bd301976bb49e449c65382
Preet-Sojitra <preet.dev373@gmail.com>
2022-11-03 21:20:26 +0530

    another attempt to fix routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Contact from "./Components/Contact"
 import FirstPage from "./Pages/FirstPage"
 import React from "react"
-import {Routes, Route, HashRouter} from "react-router-dom"
+import {Routes, Route, HashRouter, Switch} from "react-router-dom"
 import CommingSoon from "./Components/CommingSoon"
 import Committee from "./Components/Committee"
 import Theme from "./Components/Theme"
@@ -9,11 +9,11 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route exact path="/conference" element={<FirstPage />} />
-        <Route exact path="/conference/committee" element={<Committee />} />
-        <Route exact path="/conference/themes" element={<Theme />} />
-        <Route exact path="/conference/commingsoon" element={<CommingSoon />} />
-        <Route exact path="/conference/contact" element={<Contact />} />
+        <Route exact path="/" element={<FirstPage />} />
+        <Route exact path="/committee" element={<Committee />} />
+        <Route exact path="/themes" element={<Theme />} />
+        <Route exact path="/commingsoon" element={<CommingSoon />} />
+        <Route exact path="/contact" element={<Contact />} />
       </Routes>
     </HashRouter>
   )
