11674e0a6d1929ce4fd605978625bd760fba5cfb
bhagirath-bhp <bhagirath.bhp@gmail.com>
2022-11-06 23:01:29 +0530

    Updated Registration Fee Component

--- src/App.js
+++ src/App.js
@@ -9,13 +9,12 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route exact path="/" element={<RegistrationFeePage />} />
-        {/* <Route exact path="/" element={<FirstPage />} />
+        <Route exact path="/" element={<FirstPage />} />
         <Route exact path="/conference/committee" element={<Committee />} />
         <Route exact path="/conference/themes" element={<Theme />} />
         <Route exact path="/conference/commingsoon" element={<CommingSoon />} />
         <Route exact path="/conference/contact" element={<ContactPage />} />
-        <Route exact path="/conference/registration" element={<RegistrationFeePage />} /> */}
+        <Route exact path="/conference/registration" element={<RegistrationFeePage />} />
       </Routes>
     </HashRouter>
   )
