ef8b3005334bdfad6095f16528ee881609efdbe8
Preet Sojitra <preet.dev373@gmail.com>
2022-11-15 23:52:05 +0530

    fix contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,38 +2,40 @@ import "../assets/css/contact.css"
 
 const Contact = () => {
   return (
-    <>
-      <div className="contactus">
-        <div className="con-title">
-          <h1>Contacts</h1>
-        </div>
-        <div className="con-content">
-          <ul>
-            <li>
-              <p>General Inquiries</p>
-              <p>Conference Chair, IEEE GlobConHT 2023</p>
-              <a className="url" href="mailto:r.n.s@ieee.org">
-                Email Id: r.n.s@ieee.org
-              </a>
-            </li>
-            <li>
-              <p>Technical & Paper Submissions: </p>
-              <p>Technical Chair, IEEE GlobConHT 2023</p>
-              <a className="url" href="mailto:globconht@gmail.com">
-                Email Id: globconht@gmail.com
-              </a>
-            </li>
-            <li>
-              <p>Special Sessions, Tutorials, Workshops Proposals : </p>
-              <p>Conference Secretary, IEEE GlobConHT 2023</p>
-              <a className="url" href="mailto:university_conference@yahoo.com">
-                Email Id: university_conference@yahoo.com
-              </a>
-            </li>
-          </ul>
-        </div>
+    <div className="contactus">
+      <div className="con-title">
+        <h1>Contacts</h1>
       </div>
-    </>
+      <div className="con-content">
+        <ul>
+          <li>
+            <p>General Inquiries & Sponsership</p>
+            <a className="url" href="mailto:Green.hydrogen@pdpu.ac.in">
+              Email Id: Green.hydrogen@pdpu.ac.in
+            </a>
+          </li>
+          <li>
+            <p>Technical & Paper Submissions: </p>
+            <a className="url" href="mailto:greenhydrogenpdpu@gmail.com">
+              Email Id: greenhydrogenpdpu@gmail.com
+            </a>
+          </li>
+          <li>
+            <p className="student-organisers">Student Organisers</p>
+            <p className="student-name">Hari Pavan Sriram Yalamati</p>
+            <a className="url" href="mailto:Hari.yphd21@spt.pdpu.ac.in">
+              Email Id: Hari.yphd21@spt.pdpu.ac.in
+            </a>
+            <p>Phone No. +919908242145 </p>
+            <p className="student-name student2">Sneha Sandeep Lavate</p>
+            <a className="url" href="mailto:Sneha.lphd20@spt.pdpu.ac.in">
+              Email Id: Sneha.lphd20@spt.pdpu.ac.in
+            </a>
+            <p>Phone No. : +917218511311</p>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
