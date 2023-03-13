4a445ed944a81fdf8910fe86be71ded872df64c5
Aniket Patel <patelaniket1207@gmail.com>
2023-03-13 15:08:29 +0530

    Added Google maps in news section

--- src/Components/Awards.js
+++ src/Components/Awards.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect,useState} from "react";
 import "../assets/css/Awards.css";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import "../assets/css/Awards.css";
 
 const Awards = () => {
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
+
   return (
     <>
       <Navbar />
+      <div className="weather-section">
+        <h1>Current Weather</h1>
+        <div className="weather-container">
+            {/* {(typeof data.main != 'undefined') ? (
+            <Weather weatherData={data}/>
+          ): (
+            <div></div>
+          )} */}
+        </div>
+      </div>
       <div className="map-title"><h1>Maps</h1></div>
       <div className="mapouter ">
         <div className="gmap_canvas">
